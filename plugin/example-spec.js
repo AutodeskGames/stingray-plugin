9fcba81cb799a6f1250769308128230a08bd3727
Jonathan Schmidt <jschmidt42@gmail.com>
2017-04-11 15:39:58 -0400

    Add example test spec

---
+++ plugin/example-spec.js
@@ -0,0 +1,29 @@
+/* jshint jasmine: true */
+/* globals example, it, describe, expect */
+describe("Plugin Example", function(require){
+    "use strict";
+
+    const stingray = require('stingray');
+
+    const CONFIG = 'dev';
+    //const CONFIG = 'debug'; // Uncomment this line if you want to test the debug version of your DLL.
+    const DLL_PATH = require.toUrl(`example/binaries/editor/win64/${CONFIG}/editor_plugin_w64_${CONFIG}.dll`);
+
+    var pluginId = null;
+
+    it("load native extension", function () {
+        expect(stingray.fs.exists(DLL_PATH)).toBeTruthy();
+        pluginId = stingray.loadNativeExtension(DLL_PATH);
+        
+        expect(pluginId).toBeDefined();
+        expect(example).toBeDefined();
+    });
+
+    it("TODO: native function call tests", function () {
+        //expect(example.test_call).toBeDefined();
+    });
+
+    it("unload native extension", function () {
+        expect(stingray.unloadNativeExtension(pluginId)).toBeTruthy();
+    });
+});
